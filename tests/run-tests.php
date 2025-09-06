cd6ab74640c78d5f5804db4138572cc8fef049d3
Dat Hoang <htdat@users.noreply.github.com>
2025-09-06 14:29:14 +0700

    fix

--- tests/run-tests.php
+++ tests/run-tests.php
@@ -308,7 +308,7 @@ class FlickrXMLTest
             'job-is-both-fun-and-colorful',
             'xanh-xanh-mat-mat',
             'lang-le-hoang-hon',
-            'humble-speakers-wrongggg',
+            'humble-speakers',
             'candlelit-paper-flowers-or-no-rubbish',
             'hue-it-is',
             'childhood-moment',
